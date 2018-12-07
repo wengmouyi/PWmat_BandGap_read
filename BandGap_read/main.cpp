542e97464a4b5a0d15e269ec5774d368a8eb608d
Mouyi Weng <wengmouyi@wengmouyidemacbook-pro.local>
2018-12-07 12:33:59 +0800

    Initial Commit

---
+++ BandGap_read/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BandGap_read
+//
+//  Created by 翁谋毅 on 2018/12/7.
+//  Copyright © 2018 翁谋毅. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
